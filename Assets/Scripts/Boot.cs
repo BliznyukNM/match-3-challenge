3f5e7accbbf26208c5c50d11cfec7f42b86ee20e
Nikita Bliznyuk <bliznyuknm@gmail.com>
2021-10-18 10:59:06 +0200

    [Assignment 1] added a yellow piece

--- Assets/Scripts/Boot.cs
+++ Assets/Scripts/Boot.cs
@@ -11,12 +11,12 @@ namespace Tactile.TactileMatch3Challenge {
 		void Start () {
 			
 			int[,] boardDefinition = {
-				{3, 3, 1, 2, 3, 3},
-				{2, 2, 1, 2, 3, 3},
+				{3, 3, 1, 2, 4, 4},
+				{2, 2, 1, 2, 3, 4},
 				{1, 1, 0, 0, 2, 2},
 				{2, 2, 0, 0, 1, 1},
-				{1, 1, 2, 2, 1, 1},
-				{1, 1, 2, 2, 1, 1},
+				{1, 4, 2, 2, 1, 1},
+				{1, 4, 2, 2, 1, 1},
 			};
 
 			var pieceSpawner = new PieceSpawner();
